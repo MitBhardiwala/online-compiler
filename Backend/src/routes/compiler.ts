373ceb3172fd32907596864074fbe552766b65d8
Mit0921 <meetbhardiwala@gmail.com>
2026-07-26 19:55:45 +0530

    updated folder structure

--- Backend/src/routes/compiler.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { executeCode } from '../controllers/compilerController.js';
-const router = express.Router();
-
-router.post('/execute', executeCode);
-
-export default router;
--- Backend/src/routes/email.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { sendSuggestionEmail } from '../controllers/emailController.js';
-const router = express.Router();
-
-router.post('/suggestions', sendSuggestionEmail);
-
-export default router;
